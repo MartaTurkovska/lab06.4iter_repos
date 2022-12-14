6b6a67a9d8ef489bebc76bf4133d349091ee8c5f
Marta <turkovska05@gmail.com>
2022-12-14 22:51:32 +0200

    hochu spaty

---
+++ UnitTest6.4iter/UnitTest6.4iter.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6.4(iter)/lab_06.4iter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest64iter
+{
+	TEST_CLASS(UnitTest64iter)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int c;
+			int a[4]{-1, 2, 5, 8};
+			c = Sum(a, 4);
+			Assert::AreEqual(c, 0);
+		}
+	};
+}
